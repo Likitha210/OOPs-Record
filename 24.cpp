f9bc278eec75e3a64a22e68359d8eaeac11177a1
Likitha210 <11249A147@kanchiuniv.ac.in>
2025-11-05 22:11:48 -0800

    Add files via upload

---
+++ 24.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter number of elements:";
+    cin>>n;
+    int* arr=new int;
+    cout<<"Enter"<<n<<"elements:\n";
+    for(int i=0;i<n;i++)
+    cin>>arr[i];
+    cout<<"The elements are:";
+    for(int i=0;i<n;i++)
+    cout<<arr[i]<<" ";
+    delete[]arr;
+    return 0;
+}
